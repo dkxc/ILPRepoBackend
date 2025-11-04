b740d0d8257aec49396d69058ba21a6fc1526567
ALEXperion <232743092+ALEXperion@users.noreply.github.com>
2025-11-04 16:04:31 +0530

    Admin Dashboard

--- IlpRepoBackend/IlpRepoBackend.Domain/Entities/TrainingSchedule.cs
+++ IlpRepoBackend/IlpRepoBackend.Domain/Entities/TrainingSchedule.cs
@@ -4,8 +4,8 @@ namespace IlpRepoBackend.Domain.Entities
     {
         public int Id { get; set; }
         public int? BatchId { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Category { get; set; } = string.Empty;
+        //public string Title { get; set; } = string.Empty;
+        //public string Category { get; set; } = string.Empty;
         public DateTime TrainingDate { get; set; }
         public int Hours { get; set; }
         public DateTime CreatedAt { get; set; }
